75e8c854ccaa830225f0016f7be2760e0e6ec2ed
Nabeil <nabeillemar@gmail.com>
2021-11-24 17:43:54 -0800

    commit tree 456562961cb5b60ac3afffd310fb210b90a14012

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your code here
+
+function submitData(userName, userEmail) {
+    let formData = {
+      name: userName,
+      email: userEmail
+    }
+  
+    let configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+
+      body: JSON.stringify(formData)
+    };
+  
+
+    return fetch("http://localhost:3000/users", configObj)
+
+      .then(function(response) {
+
+        return response.json();
+      })
+
+      .then(function(object) {
+
+        console.log(object); 
+        document.body.innerHTML = `<p>id: ${object.id}</p><br><p>name: ${object.name}`
+      })
+ 
+      .catch(function(error) {
+        alert("Bad things!");
+        console.log(error.message);
+        document.body.innerHTML = error.message
+      });
+  }
